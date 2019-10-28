7392cb60d1c95181b0e53729c7f19207f5065733
Nick Anich <47158373+nmanich@users.noreply.github.com>
2019-10-28 11:30:12 -0500

    Update auk filter by portal and date.R

--- auk filter by portal and date.R
+++ auk filter by portal and date.R
@@ -15,16 +15,16 @@ ebird_data <- auk_ebd("ebd_US-WI_201412_201912_relAug-2019.txt") %>% #point to i
   read_ebd()
 
 -------------------------------------------
-#this is how it works with pipes
+#this is how it works with pipes, probably a better way to make the dataframe
 
 library(auk)
 
- ebird_data <- auk_ebd("ebd_US-WI_201412_201912_relAug-2019.txt") %>% 
+output_file <- "allatlas2priorityspecies.txt" #name your output file
+ebird_data <- auk_ebd("ebd_US-WI_201412_201912_relAug-2019.txt") %>% 
 
   auk_project(project = "EBIRD_ATL_WI")  %>%
-
   auk_date(date = c("2015-01-01", "2019-12-31")) %>%
-
-  auk_filter(file = "2019aug_atlaswi.txt") %>% 
+  auk_species(species = c("Acadian Flycatcher", "American Bittern", "American Wigeon", "Bay-breasted Warbler", "Bell's Vireo", "Black Tern", "Black-crowned Night-Heron", "Black-necked Stilt", "Boreal Chickadee",  "Bufflehead", "Carolina Wren", "Caspian Tern", "Cattle Egret", "Cerulean Warbler", "Common Goldeneye", "Common Nighthawk", "Common Tern", "Connecticut Warbler", "Eared Grebe", "European Goldfinch", "Forster's Tern", "Golden-winged Warbler", "Great Black-backed Gull", "Great Egret", "Great Tit", "Greater Prairie-Chicken", "Henslow's Sparrow", "Hooded Warbler", "Horned Grebe", "Kentucky Warbler", "King Rail", "Kirtland's Warbler", "Lark Sparrow", "LeConte's Sparrow", "Least Bittern", "Loggerhead Shrike", "Long-eared Owl", "Nelson's Sparrow", "Northern Bobwhite", "Northern Goshawk", "Northern Pintail", "Olive-sided Flycatcher", "Peregrine Falcon", "Philadelphia Vireo", "Piping Plover", "Purple Martin", "Red-necked Grebe", "Red-shouldered Hawk", "Ruby-crowned Kinglet", "Sharp-tailed Grouse", "Short-eared Owl", "Snowy Egret", "Spruce Grouse", "Swainson's Thrush", "Tennessee Warbler", "Upland Sandpiper", "Western Meadowlark", "Western Grebe", "Whooping Crane", "Wilson's Phalarope", "Wilson's Warbler", "Worm-eating Warbler", "Yellow Rail", "Yellow-breasted Chat", "Yellow-crowned Night-Heron", "Yellow-headed Blackbird", "Yellow-throated Warbler")) %>%
+  auk_filter(file = output_file) %>% 
 
   read_ebd()
