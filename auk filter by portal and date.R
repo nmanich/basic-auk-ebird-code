a6761bcb1fce29543735a23b0724db8178b92387
Nick Anich <47158373+nmanich@users.noreply.github.com>
2020-07-01 11:19:54 -0500

    Update auk filter by portal and date.R

--- auk filter by portal and date.R
+++ auk filter by portal and date.R
@@ -3,9 +3,8 @@ library(auk) #load auk (auk will not run unless you have cygwin on your computer
 # Setup input and output files, make sure you set working directory
 
 output_file <- "2018atlas.txt" #name your output file
-ebird_data <- auk_ebd("ebd_US-WI_201412_201912_relAug-2019.txt") %>% #point to input file
   # 1. reference file
-  auk_ebd() %>% 
+  ebird_data <- auk_ebd("ebd_US-WI_201412_201912_relAug-2019.txt") %>% #point to input file
   # 2. define filters
   auk_project(project = "EBIRD_ATL_WI")  %>% #this filters it to only the WI atlas portal data
   auk_date(date = c("2018-01-01", "2018-12-31")) %>% #this filters it to a specific date window
